d7d873e9ce2ffacdd36b905f86f98851c4b8fec2
ssenyonjo <gssenyonjo@gmail.com>
2020-12-23 15:32:50 +0300

    deployed to firebase

--- src/functions.js
+++ src/functions.js
@@ -18,16 +18,16 @@ export function getIdFromPath (position) {
 
 export function initializeFirebase() {
     // Generated from https://console.firebase.google.com/project/tsc-feedback/settings/general/web:ODhjMWNlMDktMDc4ZS00ZWZlLTg1MjItMTY5NTIyNTFlOTg5
-      const firebaseConfig = {
-        apiKey: "AIzaSyAiFmYX7tWxC5F6amDtJnDiOfxzipveCoY",
-        authDomain: "milzcollection.firebaseapp.com",
-        databaseURL: "https://milzcollection.firebaseio.com",
-        projectId: "milzcollection",
-        storageBucket: "milzcollection.appspot.com",
-        messagingSenderId: "366091822440",
-        appId: "1:366091822440:web:9e2380ed7b9649ac0962dd",
-        measurementId: "G-WCRBDJZ4D2"
-      };
+    const firebaseConfig = {
+      apiKey: "AIzaSyBFmGQu9lVqNZKFRBz05vuvn3VAU829CGE",
+      authDomain: "education-0.firebaseapp.com",
+      databaseURL: "https://education-0.firebaseio.com",
+      projectId: "education-0",
+      storageBucket: "education-0.appspot.com",
+      messagingSenderId: "540449373743",
+      appId: "1:540449373743:web:b61c805bd3fc050040d0da",
+      measurementId: "G-L2FL8956E6"
+    };
     // Initialize Firebase
     if(!firebase.apps.length) {
         firebase.initializeApp(firebaseConfig);
